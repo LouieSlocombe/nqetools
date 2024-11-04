04c247e37cd93533c14850f100481f1c4fa54e49
Louie Slocombe <louies@hotmail.co.uk>
2024-11-04 14:54:13 -0700

    cleaned up the tools

--- nqetools/tools.py
+++ nqetools/tools.py
@@ -1,24 +1,40 @@
 import os
 import sys
+
 from scipy.constants import physical_constants
 
+
 def add_ipi_paths(base=os.path.expanduser("~") + "/i-pi/"):
-    """ Adds to system paths so that one can run from the jupyter
-    notebooks without any settings after having just cloned the i-PI repo
-    and compiled the FORTRAN driver. """
+    """
+    Adds i-PI paths to the system path and environment variables.
+
+    Parameters:
+    base (str, optional): The base directory for i-PI. Default is the i-PI directory in the user's home directory.
+
+    Returns:
+    None
+    """
     sys.path.append(base)
-    os.environ['PATH'] += (":" + base + "/bin/")
+    os.environ['PATH'] += f":{base}/bin/"
+    return None
 
 
 def rm_ipi_tmp(tmp=r"/tmp/ipi_localhost"):
+    """
+    Removes the i-PI temporary file if it exists.
+
+    Parameters:
+    tmp (str, optional): The path to the temporary file. Default is "/tmp/ipi_localhost".
+
+    Returns:
+    None
+    """
     if os.path.exists(tmp):
         try:
             os.remove(tmp)
         except FileNotFoundError:
             pass
     return None
-
-
 
 
 # Conversion factor from Bohr to Angstrom
