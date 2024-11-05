054fea9132c2ee1ed8a0d2863f10251f946f82a0
Louie Slocombe <louies@hotmail.co.uk>
2024-11-05 10:55:40 -0700

    added outline of tests

---
+++ tests/general.py
@@ -0,0 +1,12 @@
+import nqetools as nqe
+
+"""
+Test the exe functions for simple cases
+optimise water
+md of water and confirm and output file
+vibrate water
+TS of water
+
+Check that the other calculators work
+
+"""
---
+++ tests/xml.py
@@ -0,0 +1,7 @@
+import nqetools as nqe
+import xml.etree.ElementTree as ET
+
+"""
+Check that all the modifiers work
+
+"""
