a9440205d08692f41d50444355d95bc0fca074e2
Louie Slocombe <louies@hotmail.co.uk>
2024-11-10 22:50:36 -0700

    updated example

--- examples/ma/malonaldehyde.py
+++ examples/ma/malonaldehyde.py
@@ -15,101 +15,20 @@ import nqetools as nqe
 from nqetools import get_ts_image, get_neb_path
 
 
-def add_hydrogen_halfway(atoms, index1, index2):
-    """
-    Add a hydrogen atom halfway between two atoms in an Atoms object.
-
-    Parameters:
-    atoms (Atoms): The ASE Atoms object.
-    index1 (int): The index of the first atom.
-    index2 (int): The index of the second atom.
-
-    Returns:
-    Atoms: The updated Atoms object with the hydrogen atom added.
-    """
-    atoms = atoms.copy()
-    # Get the positions of the two atoms
-    pos1 = atoms.positions[index1]
-    pos2 = atoms.positions[index2]
-
-    # Calculate the midpoint
-    midpoint = (pos1 + pos2) / 2.0
-
-    # Add a hydrogen atom at the midpoint
-    atoms += Atoms('H', positions=[midpoint])
-
-    return atoms
 
 
-def add_hydrogen_at_distance(atoms, index1, index2, distance):
-    """
-    Add a hydrogen atom at a specified distance from one atom along the line between two atoms in an Atoms object.
-
-    Parameters:
-    atoms (Atoms): The ASE Atoms object.
-    index1 (int): The index of the first atom.
-    index2 (int): The index of the second atom.
-    distance (float): The distance from the first atom to place the hydrogen atom.
-
-    Returns:
-    Atoms: The updated Atoms object with the hydrogen atom added.
-    """
-    atoms = atoms.copy()
-    # Get the positions of the two atoms
-    pos1 = atoms.positions[index1]
-    pos2 = atoms.positions[index2]
-
-    # Calculate the direction vector from atom1 to atom2
-    direction = pos2 - pos1
-    direction /= np.linalg.norm(direction)  # Normalize the direction vector
-
-    # Calculate the position of the hydrogen atom
-    hydrogen_position = pos1 + direction * distance
-
-    # Add a hydrogen atom at the calculated position
-    atoms += Atoms('H', positions=[hydrogen_position])
-
-    return atoms
-
-
-def swap_bonding_configuration(atoms, donor_index, hydrogen_index, acceptor_index):
-    """
-    Swap the bonding configuration from O-H...O to O...H-O in an Atoms object.
-
-    Parameters:
-    atoms (Atoms): The ASE Atoms object.
-    donor_index (int): The index of the donor oxygen atom.
-    hydrogen_index (int): The index of the hydrogen atom.
-    acceptor_index (int): The index of the acceptor oxygen atom.
-
-    Returns:
-    Atoms: The updated Atoms object with the swapped bonding configuration.
-    """
-    atoms = atoms.copy()
-    # Get the positions of the donor, hydrogen, and acceptor atoms
-    donor_pos = atoms.positions[donor_index]
-    hydrogen_pos = atoms.positions[hydrogen_index]
-    acceptor_pos = atoms.positions[acceptor_index]
-
-    # Calculate the new position for the hydrogen atom
-    direction = acceptor_pos - donor_pos
-    direction /= np.linalg.norm(direction)  # Normalize the direction vector
-    new_hydrogen_pos = acceptor_pos - direction * np.linalg.norm(hydrogen_pos - donor_pos)
-
-    # Update the position of the hydrogen atom
-    atoms.positions[hydrogen_index] = new_hydrogen_pos
-
-    return atoms
-
-
-fmax = 0.01
-n_images = 7
-calc = mace_off(model="large")
+fmax = 0.05
+n_images = 5
+# calc = mace_off(model="large")
+calc = mace_anicc()
 
 # atoms = pubchem_atoms_search(smiles="C(C=O)C=O")
 atoms = read("malonaldehyde.traj")
 # delete the hydrogen atom
 del atoms[-1]
+
+del atoms[5]
+
 atoms = nqe.optimise_geom(atoms, calc, fmax=fmax)
 
 # product = swap_bonding_configuration(reactant,0, 9,1)
@@ -123,7 +42,6 @@ product = nqe.optimise_geom(product, calc, fmax=fmax)
 view(product)
 
 neb = nqe.prepare_neb(reactant, product, calc, n_images=n_images)
-view(neb.images)
 
 ts_path = nqe.optimise_neb(neb, fmax=fmax, n_images=n_images)
 view(ts_path)
@@ -132,3 +50,6 @@ ts_image = nqe.get_ts_image(ts_path, calc)
 view(ts_image)
 
 nqe.plot_neb(ts_path, calc)
+
+vib = nqe.get_vibrations(ts_image,calc)
+print(vib,flush=True)
