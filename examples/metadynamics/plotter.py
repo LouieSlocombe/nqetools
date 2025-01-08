da8e1ed01960c6e7f365c2efb9d7e57f14dd26a5
Louie Slocombe <louies@hotmail.co.uk>
2025-01-07 22:55:35 -0700

    updated plotting tools

--- examples/metadynamics/plotter.py
+++ examples/metadynamics/plotter.py
@@ -1,28 +1,78 @@
 import subprocess
+
 import matplotlib.pyplot as plt
-from matplotlib import colormaps
 import numpy as np
+from matplotlib import colormaps
 
 
-# Import free energy and reshape with the number of bins defined in the
-# reconstruction process.
-scm = np.loadtxt('COLVAR', usecols=1)
-tcm = np.loadtxt('COLVAR', usecols=2)
+def plot_scm_tcm(file='COLVAR'):
+    """
+    Plot the SCM vs TCM from a given file.
 
-# Plot
-fig, ax = plt.subplots(figsize=(10, 9))
+    Parameters:
+    file (str): The name of the file containing the SCM and TCM data. Default is 'COLVAR'.
 
-# Plot free energy surface
-plt.plot(scm, tcm, 'o')
-# Plot parameters
-ax.set_xlabel('SCM', fontsize=40)
-ax.set_ylabel('TCM', fontsize=40)
-ax.tick_params(axis='y', labelsize=25)
-ax.tick_params(axis='x', labelsize=25)
+    Returns:
+    None
+    """
+    # Import free energy and reshape with the number of bins defined in the
+    # reconstruction process.
+    scm = np.loadtxt(file, usecols=1)
+    tcm = np.loadtxt(file, usecols=2)
 
-plt.tight_layout()
-plt.show()
+    # Plot
+    fig, ax = plt.subplots(figsize=(10, 9))
 
+    # Plot free energy surface
+    plt.plot(scm, tcm, 'o')
+    # Plot parameters
+    ax.set_xlabel('SCM', fontsize=40)
+    ax.set_ylabel('TCM', fontsize=40)
+    ax.tick_params(axis='y', labelsize=25)
+    ax.tick_params(axis='x', labelsize=25)
+
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_fes(file='fes.dat'):
+    """
+    Plot the free energy surface from a given file.
+
+    Parameters:
+    file (str): The name of the file containing the free energy data. Default is 'fes.dat'.
+
+    Returns:
+    None
+    """
+    # Import free energy and reshape with the number of bins defined in the
+    # reconstruction process.
+    scm = np.loadtxt(file, usecols=0).reshape(301, 301)
+    tcm = np.loadtxt(file, usecols=1).reshape(301, 301)
+    fes = np.loadtxt(file, usecols=2).reshape(301, 301)
+
+    # Plot
+    fig, ax = plt.subplots(figsize=(10, 9))
+
+    # Plot free energy surface
+    im = ax.contourf(scm, tcm, fes, 10, cmap=colormaps['Blues_r'])
+    cp = ax.contour(scm, tcm, fes, 10,
+                    linestyles='-', colors='darkgray', linewidths=1.2)
+
+    # Plot parameters
+    ax.set_xlabel('SCM', fontsize=40)
+    ax.set_ylabel('TCM', fontsize=40)
+    ax.tick_params(axis='y', labelsize=25)
+    ax.tick_params(axis='x', labelsize=25)
+    cbar = fig.colorbar(im, ax=ax)
+    cbar.set_label(label=r'FES[$\epsilon$]', fontsize=40)
+    cbar.ax.tick_params(labelsize=32)
+
+    plt.tight_layout()
+    plt.show()
+
+
+plot_scm_tcm('COLVAR')
 
 # Reconstruct the free energy from HILLS using the plumed tool "sum_hills" :
 p = subprocess.Popen("plumed sum_hills --hills HILLS --outfile fes.dat" +
@@ -32,27 +82,4 @@ p.wait()
 
 # Import free energy and reshape with the number of bins defined in the
 # reconstruction process.
-scm = np.loadtxt('fes.dat', usecols=0).reshape(301, 301)
-tcm = np.loadtxt('fes.dat', usecols=1).reshape(301, 301)
-fes = np.loadtxt('fes.dat', usecols=2).reshape(301, 301)
-
-# Plot
-fig, ax = plt.subplots(figsize=(10, 9))
-
-# Plot free energy surface
-im = ax.contourf(scm, tcm, fes, 10, cmap=colormaps['Blues_r'])
-cp = ax.contour(scm, tcm, fes, 10,
-                linestyles='-', colors='darkgray', linewidths=1.2)
-
-# Plot parameters
-ax.set_xlabel('SCM', fontsize=40)
-ax.set_ylabel('TCM', fontsize=40)
-ax.tick_params(axis='y', labelsize=25)
-ax.tick_params(axis='x', labelsize=25)
-cbar = fig.colorbar(im, ax=ax)
-cbar.set_label(label=r'FES[$\epsilon$]', fontsize=40)
-cbar.ax.tick_params(labelsize=32)
-
-plt.tight_layout()
-plt.savefig('fes.png', dpi=300)
-plt.show()
+plot_fes('fes.dat')
--- examples/metadynamics/postpro.py
+++ examples/metadynamics/postpro.py
@@ -5,13 +5,29 @@ from ase.io import read
 
 
 def plumed_postpro(file_traj, setup, timestep, temperature):
+    """
+    Perform post-processing using PLUMED on a given trajectory file.
+
+    Parameters:
+    file_traj (str): The name of the trajectory file to read.
+    setup (list): The PLUMED input setup.
+    timestep (float): The timestep for the simulation in femtoseconds.
+    temperature (float): The temperature for the simulation in Kelvin.
+
+    Returns:
+    None
+    """
+    # Read the trajectory from the file
     traj = read(file_traj, index=':')
+    # Get the first frame of the trajectory
     atoms = traj[0]
+    # Set up the PLUMED calculator with the IdealGas model and the provided parameters
     calc = Plumed(calc=IdealGas(),
                   input=setup,
                   timestep=timestep,
                   atoms=atoms,
                   kT=temperature * units.kB)
+    # Write the PLUMED files for the trajectory
     calc.write_plumed_files(traj)
     return None
 
