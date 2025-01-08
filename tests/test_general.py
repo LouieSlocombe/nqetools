ae4fddd81cf114e767297fcb16628a1f10961940
Louie Slocombe <louies@hotmail.co.uk>
2025-01-07 19:38:50 -0700

    updated the test to check orca works

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,4 +1,7 @@
 import nqetools as nqe
+import ase.build
+import time
+from mace.calculators import mace_anicc
 
 """
 Test the exe functions for simple cases
@@ -16,3 +19,34 @@ def test_calculate_nbeads():
     # For water omega_max ~ 3800 invcm this number is ~ 18 at 300 K. So a safe choice is 32 replicas
     n_beads = nqe.calculate_nbeads(3800.0, 300.0)
     assert n_beads == 18
+
+
+def test_orca_calc():
+    orca_path = '/home/louie/orca_6_0_1/orca'
+
+    # build the molecule
+    atoms = ase.build.molecule('H2O')
+    calc = nqe.orca_calc_preset(orca_path=orca_path, nprocs=1)
+
+    # set the calculator
+    atoms.calc = calc
+    # run the calculation
+    t1 = time.time()
+    energy = atoms.get_potential_energy()
+    t2 = time.time()
+    print(f"Energy: {energy} Time: {t2 - t1}")
+    pass
+
+
+def test_mace_annic():
+    # build the molecule
+    atoms = ase.build.molecule('H2O')
+    calc = mace_anicc()
+    # set the calculator
+    atoms.calc = calc
+    # run the calculation
+    t1 = time.time()
+    energy = atoms.get_potential_energy()
+    t2 = time.time()
+    print(f"Energy: {energy} Time: {t2 - t1}")
+    pass
