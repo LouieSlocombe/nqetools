f4ecf3ced826dd334bc93fd5ef8e974cd11491b9
Louie Slocombe <louies@hotmail.co.uk>
2025-01-19 16:15:54 -0700

    added test for md

---
+++ tests/test_md.py
@@ -0,0 +1,14 @@
+import time
+import os
+
+import ase.build
+from mace.calculators import mace_anicc, mace_off
+from ase.io import write, read
+import nqetools as nqe
+from ase.visualize import view
+
+def test_nve():
+    pass
+
+def test_nvt():
+    pass
