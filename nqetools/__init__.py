4d217cab26e2ce6997497b9618068d5eda210c53
Louie Slocombe <louies@hotmail.co.uk>
2025-01-20 16:51:52 -0700

    cleaned up imports

--- nqetools/__init__.py
+++ nqetools/__init__.py
@@ -5,19 +5,33 @@ from .calcs import (correlate,
                     temp_cross,
                     calculate_nbeads,
                     calc_kappa)
-
+from .calculators import (nwchem_calc_preset,
+                          orca_calc_preset,
+                          qchem_calc_preset,
+                          orca_preset_dft_cheap,
+                          orca_preset_dft_gold,
+                          orca_preset_xtb,
+                          orca_preset_mp2_gold,
+                          orca_preset_ccsd_gold,
+                          )
+from .conversions import (bohr_to_angstrom,
+                          A_to_nm,
+                          eV_to_J,
+                          J_to_kJ,
+                          avo_num,
+                          eV_to_kJpermol,
+                          eVperA2_to_kJpermolpernm2,
+                          )
 from .driver import (write_mace_driver,
                      write_nwchem_driver,
                      write_orca_driver,
                      prep_driver)
-
 from .execution import (run_ipi,
                         run_md,
                         run_optimise,
                         run_phonons,
                         run_ts,
                         run_inst)
-
 from .io import (read_ipi_xyz,
                  read_ipi_output,
                  write_xml,
@@ -29,7 +43,6 @@ from .io import (read_ipi_xyz,
                  get_final_hess,
                  copy_xyz,
                  copy_hess)
-
 from .pathway import (get_neb_path,
                       stitch_path,
                       resample_path,
@@ -52,7 +65,13 @@ from .plotting import (plot_time_energy,
                        plot_neb,
                        show_atoms)
 
+from .plumed import (write_plumed_pos,
+                     write_plumed_coord,
+                     write_plumed_dist)
+
 from .post_proc import (instanton_postproc)
+
+from .qchem_mod import (QChem)
 
 from .tools import (add_ipi_paths,
                     rm_ipi_tmp,
@@ -80,34 +99,10 @@ from .xml_parse import (update_mass,
                         update_timestep,
                         update_stride,
                         add_trajectory_centroid,
-                        add_plumed,
                         add_trajectory_plumed_extras,
-                        add_plumed_bias_section_extras,
+                        add_plumed_ff_section,
+                        add_plumed_bias_section,
+                        add_plumed,
                         )
-
-from .qchem_mod import (QChem)
-
-from .calculators import (nwchem_calc_preset,
-                          orca_calc_preset,
-                          qchem_calc_preset,
-                          orca_preset_dft_cheap,
-                          orca_preset_dft_gold,
-                          orca_preset_xtb,
-                          orca_preset_mp2_gold,
-                          orca_preset_ccsd_gold,
-                          )
-
-from .conversions import (bohr_to_angstrom,
-                          A_to_nm,
-                          eV_to_J,
-                          J_to_kJ,
-                          avo_num,
-                          eV_to_kJpermol,
-                          eVperA2_to_kJpermolpernm2,
-                          )
-
-from .plumed import (write_plumed_pos,
-                     write_plumed_coord,
-                     write_plumed_dist)
 
 __version__ = "0.1.0"
