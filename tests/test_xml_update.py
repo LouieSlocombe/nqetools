ee9d157847d3e5d21b3aaced12232612afe8406d
Louie Slocombe <louies@hotmail.co.uk>
2025-01-20 14:34:44 -0700

    updated the test function

--- tests/test_xml_update.py
+++ tests/test_xml_update.py
@@ -4,6 +4,10 @@ import xml.etree.ElementTree as ET
 import re
 
 
+def vis_xml(root):
+    nqe.write_xml(root, os.path.join(os.getcwd(), 'input.xml'))
+
+
 def count_matching_words(input_string, target_word):
     pattern = re.escape(target_word)
     return len(re.findall(pattern, input_string))
@@ -36,3 +40,21 @@ def test_timestep():
 
     # Check that the timestep is set
     assert count_matching_words(str(ET.tostring(root)), str(timestep)) == 1
+
+
+def test_plumed():
+    root = ET.parse(os.path.abspath("../templates/NVE.xml")).getroot()
+    # Update the plumed file
+    nqe.add_plumed(root)
+
+    # Write to file for visual inspection
+    # vis_xml(root)
+
+    # Check that ffplumed is in the xml
+    assert count_matching_words(str(ET.tostring(root)), "ffplumed") == 2
+
+    # Check that bias has been added to the xml
+    assert count_matching_words(str(ET.tostring(root)), "bias") == 2
+
+    # Check that smotion has been added to the xml
+    assert count_matching_words(str(ET.tostring(root)), "smotion") == 2
