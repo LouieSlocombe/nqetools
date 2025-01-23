6a283ef1d516ede8230a538b9b4cbc4eed8bed66
Louie Slocombe <louies@hotmail.co.uk>
2025-01-23 13:26:14 -0700

    added more plumed test cases

--- tests/test_plumed.py
+++ tests/test_plumed.py
@@ -55,3 +55,39 @@ def test_plumed_opes_pos():
                                   md_type="NVT",
                                   plumed_type="opes-pos")
     pass
+
+
+def test_plumed_mtd_dist():
+    print(flush=True)
+    print("Testing MACE md calculator", flush=True)
+    # build the molecule
+    atoms = ase.build.molecule('H2O')
+    atoms.center(vacuum=5.0)
+
+    directory = "mace_md"
+    # If the directory exists, remove it
+    nqe.remove_directory(directory)
+    atoms_out = nqe.run_plumed_md(directory,
+                                  atoms,
+                                  driver='ase-mace',
+                                  md_type="NVT",
+                                  plumed_type="mtd-dist")
+    pass
+
+
+def test_plumed_opes_dist():
+    print(flush=True)
+    print("Testing MACE md calculator", flush=True)
+    # build the molecule
+    atoms = ase.build.molecule('H2O')
+    atoms.center(vacuum=5.0)
+
+    directory = "mace_md"
+    # If the directory exists, remove it
+    nqe.remove_directory(directory)
+    atoms_out = nqe.run_plumed_md(directory,
+                                  atoms,
+                                  driver='ase-mace',
+                                  md_type="NVT",
+                                  plumed_type="opes-dist")
+    pass
