76d0c52baa903feb2c6b43ba18bdede2156d273f
Louie Slocombe <louies@hotmail.co.uk>
2025-01-26 19:23:19 -0700

    clean up of tests

--- tests/test_general.py
+++ tests/test_general.py
@@ -12,17 +12,6 @@ from mace.calculators import mace_anicc
 
 import nqetools as nqe
 
-"""
-Test the exe functions for simple cases
-optimise water
-md of water and confirm and output file
-vibrate water
-TS of water
-
-Check that the other calculators work
-
-"""
-
 
 def test_calculate_nbeads():
     # For water omega_max ~ 3800 invcm this number is ~ 18 at 300 K. So a safe choice is 32 replicas
