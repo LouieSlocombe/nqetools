64712d5cd01aa2339c0a67032b19f025b61b11e9
Louie Slocombe <louies@hotmail.co.uk>
2025-02-16 18:29:11 -0700

    added test for moving atoms #91

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,5 +1,7 @@
 import nqetools as nqe
 from ase.build import molecule
+from ase.visualize import view
+
 
 def test_cluster_atoms():
     print(flush=True)
@@ -21,3 +23,29 @@ def test_cluster_atoms():
     assert len(clusters[0]) == 3
     assert len(clusters[1]) == 3
     assert len(clusters) == 2
+
+
+def test_move_clusters_to_distance():
+    print(flush=True)
+
+    idx1 = 0
+    idx2 = 0
+    target_distance = 10.0  # Desired distance between the target atoms
+
+    # Create two water molecules as clusters
+    water1 = molecule("H2O")
+    water2 = molecule("H2O")
+
+    # Translate the second water molecule to an arbitrary initial position
+    water2.translate([2, 0, 0])  # Initial separation of 2 Angstroms
+
+    # Move the clusters so that the target atoms are at a new separation distance
+    moved_atoms = nqe.move_clusters_to_distance(water1, water2,
+                                                index1=idx1,
+                                                index2=idx2,
+                                                target_distance=target_distance)
+    view(moved_atoms)
+
+    # Check the distance between the target atoms
+    distance = moved_atoms.get_distance(idx1, idx2 + len(water1))
+    assert round(distance, 2) == target_distance
