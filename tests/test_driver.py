ac303662370515a10f1e12362b4549ff2eada42f
Louie Slocombe <louies@hotmail.co.uk>
2025-02-18 15:16:24 -0700

    added code to test driver #94

---
+++ tests/test_driver.py
@@ -0,0 +1,105 @@
+import ase.build
+
+import nqetools as nqe
+
+
+def test_ase_mace_driver():
+    """
+    Tests the MACE calculator using the ASE driver.
+
+    This function builds a water molecule, centers it with a vacuum of 5.0 Å,
+    sets up the MACE calculator, runs the optimization, and stores the results
+    in a specified directory.
+
+    Asserts:
+        None
+    """
+    print(flush=True)
+    print("Testing MACE calculator", flush=True)
+
+    # Build the molecule
+    atoms = ase.build.molecule('H2O')
+    atoms.center(vacuum=5.0)
+
+    # Make a directory to store everything
+    directory = "mace_opti"
+    nqe.run_optimise(directory, atoms, driver='ase-mace')
+    pass
+
+
+def test_ase_orca_driver():
+    """
+    Tests the ORCA driver using the ASE driver.
+
+    This function builds a water molecule, centers it with a vacuum of 5.0 Å,
+    sets up the ORCA driver, runs the optimization, and stores the results
+    in a specified directory.
+
+    Asserts:
+        None
+    """
+    print(flush=True)
+    print("Testing ORCA driver", flush=True)
+
+    # Build the molecule
+    atoms = ase.build.molecule('H2O')
+    atoms.center(vacuum=5.0)
+
+    # Make a directory to store everything
+    directory = "orca_opti"
+    nqe.run_optimise(directory, atoms, driver='ase-orca')
+    pass
+
+
+def test_ase_nwchem_driver():
+    """
+    Tests the NWChem driver using the ASE driver.
+
+    This function builds a water molecule, centers it with a vacuum of 5.0 Å,
+    sets up the NWChem driver, runs the optimization, and stores the results
+    in a specified directory.
+
+    Asserts:
+        None
+    """
+    print(flush=True)
+    print("Testing NWChem driver", flush=True)
+
+    # Build the molecule
+    atoms = ase.build.molecule('H2O')
+    atoms.center(vacuum=5.0)
+
+    # Make a directory to store everything
+    directory = "nwchem_opti"
+    nqe.remove_directory(directory)
+
+    # Run the optimization
+    nqe.run_optimise(directory, atoms, driver='ase-nwchem')
+    pass
+
+
+def test_nwchem_driver():
+    """
+    Tests the NWChem driver.
+
+    This function builds a water molecule, centers it with a vacuum of 5.0 Å,
+    sets up the NWChem driver, runs the optimization, and stores the results
+    in a specified directory.
+
+    Asserts:
+        None
+    """
+    print(flush=True)
+    print("Testing NWChem driver", flush=True)
+
+    # Build the molecule
+    atoms = ase.build.molecule('H2O')
+    atoms.center(vacuum=5.0)
+
+    # Make a directory to store everything
+    directory = "nwchem_opti"
+    nqe.remove_directory(directory)
+
+    # Run the optimization
+    nqe.run_optimise(directory, atoms, driver='nwchem')
+    pass
