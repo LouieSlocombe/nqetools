0bfdb0dfd6e45b9607d1e5efeceda177575cac00
Louie Slocombe <louies@hotmail.co.uk>
2025-02-19 23:32:18 -0700

    added docs to plotting

--- nqetools/plumed_analysis.py
+++ nqetools/plumed_analysis.py
@@ -4,6 +4,18 @@ from .calcs import moving_average
 
 
 def plot_time_potential_bias(data):
+    """
+    Plots the potential and ensemble bias over time.
+
+    Parameters:
+    data (dict): A dictionary containing the time series data with keys:
+        - "time": A list or array of time points.
+        - "potential": A list or array of potential energy values.
+        - "ensemble_bias": A list or array of ensemble bias values.
+
+    Returns:
+    None
+    """
     fig, ax = plt.subplots(1, 1, figsize=(4, 3), constrained_layout=True)
 
     ax.plot(
@@ -28,6 +40,18 @@ def plot_time_potential_bias(data):
 
 
 def plot_time_temperature(data, window_size=100):
+    """
+    Plots the temperature over time with a moving average.
+
+    Parameters:
+    data (dict): A dictionary containing the time series data with keys:
+        - "time": A list or array of time points.
+        - "temperature": A list or array of temperature values.
+    window_size (int, optional): The window size for the moving average. Default is 100.
+
+    Returns:
+    None
+    """
     fig, ax = plt.subplots(1, 1, figsize=(4, 3), constrained_layout=True)
     min_val = int(window_size / 2)
     max_val = -int(window_size / 2 - 1)
