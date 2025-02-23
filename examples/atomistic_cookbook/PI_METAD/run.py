8ffb2d8af9f206f1f2043f8bce02a50bcc362036
Louie Slocombe <louies@hotmail.co.uk>
2025-02-23 12:28:35 -0700

    clean up

--- examples/atomistic_cookbook/PI_METAD/run.py
+++ examples/atomistic_cookbook/PI_METAD/run.py
@@ -1,27 +1,67 @@
-import time
 import os
-
-import ase.build
-from mace.calculators import mace_anicc, mace_off
-from ase.io import write, read
+import subprocess
+import chemiscope
+import ipi
+import matplotlib.pyplot as plt
+import numpy as np
+from ase.io import read
 import nqetools as nqe
 from ase.visualize import view
 
 # This follows:
-# https://atomistic-cookbook.org/examples/path-integrals/path-integrals.html
+# https://atomistic-cookbook.org/examples/pi-metad/pi-metad.html
+
 
 if __name__ == "__main__":
     print(flush=True)
-    print("PIMD of water", flush=True)
-    # build the molecule
-    atoms = read("water_32.pdb")
+    # Make a directory to store everything
+    directory_md = "md"
+    directory_metamd = "metamd"
+    directory_metapimd = "metapimd"
+    n_beads = 8
+    timestep = 1.0
+    total_steps = 5000
+    stride = 10
+    temperature = 298
+    thermostat = 'smart_sampling_1ps_n6_w2'
+    driver_code = 'zundel'
+    md_type = "NVT-GLE"
+    plumed_type = "mtd-coord"
 
-    # calc = mace_anicc()
-    # calc = mace_off(model="small", device="cpu", default_dtype="float32")
-    # # set the calculator
-    # atoms.calc = calc
-    # # run the calculation
-    # t1 = time.time()
-    # energy = atoms.get_potential_energy()
-    # t2 = time.time()
-    # print(f"Energy: {energy} Time: {t2 - t1}")
+    # Load the initial structure
+    atoms = read("h5o2+.xyz", index=-1)
+    atoms.center(vacuum=20.0)
+    view(atoms)
+
+    # Run unbiased MD
+    # Make sure the directory is empty
+    # Run the calculation
+    nqe.remove_directory(directory_md)
+    atoms = nqe.run_md(directory_md, atoms,
+                       driver=driver_code,
+                       md_type=md_type,
+                       n_beads=1,
+                       thermostat=thermostat,
+                       timestep=timestep,
+                       total_steps=total_steps,
+                       stride=stride,
+                       temperature=temperature,
+                       )
+    view(atoms)
+
+    # Run metadynamics
+    # Make sure the directory is empty
+    # Run the calculation
+    nqe.remove_directory(directory_metamd)
+    atoms = nqe.run_plumed_md(directory_metamd, atoms,
+                              driver=driver_code,
+                              md_type=md_type,
+                              n_beads=1,
+                              thermostat=thermostat,
+                              timestep=timestep,
+                              total_steps=total_steps,
+                              stride=stride,
+                              temperature=temperature,
+                              plumed_type=plumed_type,
+                              )
+    view(atoms)
