f205135e95a7b53fe03a882d1dcb946c0995a669
Louie Slocombe <louies@hotmail.co.uk>
2025-02-23 21:29:00 -0700

    added code to convert from list bohr to A #122

--- nqetools/conversions.py
+++ nqetools/conversions.py
@@ -15,3 +15,24 @@ avo_num = const["Avogadro constant"][0]  # mol^-1
 eV_to_kJpermol = eV_to_J * J_to_kJ * avo_num
 # Conversion factor from eV/Å² to 1 kJ/(mol·nm²)
 eVperA2_to_kJpermolpernm2 = eV_to_kJpermol / A_to_nm ** 2
+
+
+def convert_atom_list_bohr_to_angstrom(atoms_list: list['Atoms']) -> list['Atoms']:
+    """
+    Convert positions and cell parameters from Bohr to Angstrom for a list of ASE Atoms objects.
+
+    Parameters:
+    atoms_list (list[ase.Atoms]): List of ASE Atoms objects with positions and cells in Bohr
+
+    Returns:
+    list[ase.Atoms]: List of ASE Atoms objects with positions and cells in Angstrom
+    """
+
+    converted_atoms = []
+    for atoms in atoms_list:
+        atoms_copy = atoms.copy()
+        atoms_copy.positions *= bohr_to_angstrom
+        atoms_copy.cell *= bohr_to_angstrom
+        converted_atoms.append(atoms_copy)
+
+    return converted_atoms
