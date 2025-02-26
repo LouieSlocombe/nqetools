ada2ad867f442d0f048c1e8dda1995303a21a1c0
Louie Slocombe <louies@hotmail.co.uk>
2025-02-26 10:42:38 -0700

    relaxed python requirement

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.12',
+    python_requires='>=3.9',
     install_requires=[
         'numpy',
         'pandas',
