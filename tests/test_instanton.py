7aff822f9725c98419b1f520d130e8bbe67d55ea
Louie Slocombe <louies@hotmail.co.uk>
2025-04-21 23:03:23 -0700

    clean up the instanton test

--- tests/test_instanton.py
+++ tests/test_instanton.py
@@ -1,29 +1,54 @@
+import os
+
 import nqetools as nqe
 
 
-def test_parse_inst_thermo_data():
-    temperature = 300
-    n_beads = 80
-    directory_instanton = f'data/instanton/inst_{n_beads}/'
-    nqe.run_instanton_post_process(directory_instanton,
-                                   process_type='instanton',
-                                   temperature=temperature,
-                                   n_beads=n_beads)
+# Fails as the file is not minimized properly
+def test_parse_react_thermo_data():
+    print(flush=True)
+    temperature = 300.0
+    directory = 'data/instanton/react_phonon/'
+    nqe.run_instanton_post_process(directory,
+                                   process_type='reactant',
+                                   temperature=temperature)
 
-    data = nqe.parse_inst_thermo_data(directory_instanton)
-
-    print(data)
+    data = nqe.parse_react_thermo_data(directory)
+    print(data, flush=True)
+    os.remove(os.path.join(directory, 'thermo_data.out'))
+    os.remove(os.path.join(directory, 'freq.dat'))
     pass
 
 
 def test_parse_ts_thermo_data():
     print(flush=True)
-    temperature = 300
-    directory_instanton = 'data/instanton/ts/'
-    nqe.run_instanton_post_process(directory_instanton, process_type='TS', temperature=temperature)
+    temperature = 300.0
+    directory = 'data/instanton/ts/'
+    nqe.run_instanton_post_process(directory,
+                                   process_type='TS',
+                                   temperature=temperature)
 
-    data = nqe.parse_ts_thermo_data(directory_instanton)
-    print(data)
+    data = nqe.parse_ts_thermo_data(directory)
+    print(data, flush=True)
+    os.remove(os.path.join(directory, 'thermo_data.out'))
+    os.remove(os.path.join(directory, 'freq.dat'))
+    pass
+
+
+def test_parse_inst_thermo_data():
+    print(flush=True)
+    temperature = 300.0
+    n_beads = 80
+    directory = f'data/instanton/inst_{n_beads}/'
+    nqe.run_instanton_post_process(directory,
+                                   process_type='instanton',
+                                   temperature=temperature,
+                                   n_beads=n_beads)
+
+    data = nqe.parse_inst_thermo_data(directory)
+
+    print(data, flush=True)
+    os.remove(os.path.join(directory, 'thermo_data.out'))
+    os.remove(os.path.join(directory, 'freq.dat'))
     pass
 
 
@@ -38,15 +63,19 @@ def test_calc_instanton_kappa():
                                    temperature=temperature)
     data_ts = nqe.parse_ts_thermo_data(directory_ts)
 
-    directory_instanton = f'data/instanton/inst_{n_beads}/'
-    nqe.run_instanton_post_process(directory_instanton,
+    directory_inst = f'data/instanton/inst_{n_beads}/'
+    nqe.run_instanton_post_process(directory_inst,
                                    process_type='instanton',
                                    temperature=temperature,
                                    n_beads=n_beads)
 
-    data_inst = nqe.parse_inst_thermo_data(directory_instanton)
+    data_inst = nqe.parse_inst_thermo_data(directory_inst)
 
     kappa = nqe.calc_instanton_kappa(data_ts, data_inst)
-    print(kappa)
-    #     [40  ,  10.127813329699068],
-    #     [80  ,   9.809437521247709]])
+
+    print('Tunneling factor, kappa = {:5.3f}'.format(kappa))
+    os.remove(os.path.join(directory_ts, 'thermo_data.out'))
+    os.remove(os.path.join(directory_ts, 'freq.dat'))
+    os.remove(os.path.join(directory_inst, 'thermo_data.out'))
+    os.remove(os.path.join(directory_inst, 'freq.dat'))
+    assert round(kappa, 3) == round(10.127813329699068, 3)
