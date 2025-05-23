c164a298db4ae08e851fecc535e47b9e6c7543fc
Louie Slocombe <louies@hotmail.co.uk>
2025-05-23 12:45:39 -0700

    checkpoint on orca client

--- tests/orca/ase_orca_client.py
+++ tests/orca/ase_orca_client.py
@@ -1,33 +1,25 @@
 #!/usr/bin/env python3
-"""
-Tiny ASE client for i-PI that evaluates energies + forces with ORCA.
-Usage:  python ase_orca_client.py [socket_name]
-If you omit the argument it defaults to "orca_ase" so that it matches
-the <address> tag in the XML.
-"""
 import os
-
-from ase.build import molecule  # only to make a placeholder Atoms object
-from ase.calculators.orca import ORCA  # ASE ↔ ORCA interface
-from ase.calculators.orca import OrcaProfile
-from ase.calculators.socketio import SocketClient  # i-PI protocol client
-
+# from mace.calculators import mace_off
+from ase.calculators.orca import ORCA, OrcaProfile
+from ase.calculators.socketio import SocketClient
+from ase.io import read
 
 unixsocket = "orca_ase"
-
 
 orca_path = os.environ.get('ORCA_PATH')
 profile = OrcaProfile(command=orca_path)
 orca_calc = ORCA(
     profile=profile,
-    label="orca_run",
-    #orcasimpleinput="PBE def2-SVP",
+    orcasimpleinput="PBE def2-SVP",
     charge=0,
     mult=1,
 )
+# mace_calc = mace_off(model='small', device='cpu', default_dtype='float32')
 
-atoms = molecule("H2O")  # any three-atom placeholder is fine
+atoms = read("water.xyz", index=0)
+# atoms.calc = mace_calc
 atoms.calc = orca_calc
-
+#print(atoms.get_potential_energy())
 client = SocketClient(unixsocket=unixsocket)
-client.run(atoms)  # blocks; i-PI will control the loop
+client.irun(atoms, use_stress=True)
