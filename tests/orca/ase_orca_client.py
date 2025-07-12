6353de5ef5df9592d40196a066b396207fe5c49d
Louie Slocombe <louies@hotmail.co.uk>
2025-07-12 10:07:48 -0700

    updated run example

--- tests/orca/ase_orca_client.py
+++ tests/orca/ase_orca_client.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import os
-# from mace.calculators import mace_off
 from ase.calculators.orca import ORCA, OrcaProfile
 from ase.calculators.socketio import SocketClient
 from ase.io import read
@@ -11,15 +10,11 @@ orca_path = os.environ.get('ORCA_PATH')
 profile = OrcaProfile(command=orca_path)
 orca_calc = ORCA(
     profile=profile,
-    orcasimpleinput="PBE def2-SVP",
+    orcasimpleinput="PBE def2-SVP EnGrad",
     charge=0,
     mult=1,
 )
-# mace_calc = mace_off(model='small', device='cpu', default_dtype='float32')
-
 atoms = read("water.xyz", index=0)
-# atoms.calc = mace_calc
 atoms.calc = orca_calc
-#print(atoms.get_potential_energy())
 client = SocketClient(unixsocket=unixsocket)
 client.irun(atoms, use_stress=True)
