ee9b2f20801e8a568cebc450a3a2f5b29d784d74
Louie Slocombe <louies@hotmail.co.uk>
2025-07-28 00:43:13 -0700

    clean up #182

--- nqetools/driver.py
+++ nqetools/driver.py
@@ -271,7 +271,7 @@ def orca_calc_preset(orca_path=None,
         charge=charge,
         mult=multiplicity,
         directory=directory,
-        orcasimpleinput=inpt_simple + ' EnGrad',
+        orcasimpleinput=inpt_simple + ' TightSCF EnGrad',
         orcablocks=inpt_blocks
     )
     return calc
