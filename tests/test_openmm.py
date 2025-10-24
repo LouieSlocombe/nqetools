32523da88b428dd8fc5983f0d00766736ea5b7d1
Louie Slocombe <louies@hotmail.co.uk>
2025-10-24 16:06:46 -0700

    Add openmm tests #249

--- tests/test_openmm.py
+++ tests/test_openmm.py
@@ -7,12 +7,10 @@ from openmmml import MLPotential
 from openff.toolkit import Molecule
 from openmmforcefields.generators import GAFFTemplateGenerator
 
-from rdkit import Chem
-from rdkit.Chem import AllChem
 
 def test_openmm_ml():
     # Load toluene structure
-    pdb = app.PDBFile("data/pdb/input_aaa.pdb")
+    pdb = app.PDBFile("tests/data/pdb/input_aaa.pdb")
 
     potential = MLPotential(
         'mace-off23-large'
@@ -45,23 +43,16 @@ def test_openmm_ml_mixed_system():
     pass
 
 
-
-
-
 def test_openmm_ff_param():
     # Create an OpenFF Molecule object for benzene from SMILES
-
 
     molecule = Molecule.from_smiles("c1ccccc1")
     # Create the GAFF template generator
 
-    molecules = Molecule.from_file("molecules.sdf")
-
+    # molecules = Molecule.from_file("molecules.sdf")
 
     gaff = GAFFTemplateGenerator(molecules=molecule)
     # Create an OpenMM ForceField object with AMBER ff14SB and TIP3P with compatible ions
-
-
     forcefield = app.ForceField(
         "amber/protein.ff14SB.xml",
         "amber/tip3p_standard.xml",
@@ -69,14 +60,49 @@ def test_openmm_ff_param():
     )
     # Register the GAFF template generator
     forcefield.registerTemplateGenerator(gaff.generator)
-    # You can now parameterize an OpenMM Topology object that contains the specified molecule.
-    # forcefield will load the appropriate GAFF parameters when needed, and antechamber
-    # will be used to generate small molecule parameters on the fly.
-    from openmm.app import PDBFile
-
-    pdbfile = PDBFile("t4-lysozyme-L99A-with-benzene.pdb")
-    system = forcefield.createSystem(pdbfile.topology)
 
 
 def test_openmm_rpmd():
-    pass
+    pdb = app.PDBFile("tests/data/pdb/input_aaa.pdb")
+    forcefield = app.ForceField('amber99sb.xml', 'tip3p.xml')
+
+    modeller = app.Modeller(pdb.topology, pdb.positions)
+    modeller.deleteWater()
+    modeller.addHydrogens(forcefield)
+
+    modeller.addSolvent(forcefield,
+                        padding=2.0 * unit.nanometer)
+
+    system = forcefield.createSystem(
+        modeller.topology,
+        nonbondedMethod=app.PME,
+        nonbondedCutoff=1.0 * unit.nanometer,
+    )
+
+    # # Run langevin dynamics at 300K for 1000 steps
+    # integrator = openmm.LangevinIntegrator(300 * unit.kelvin,
+    #                                        1.0 / unit.picoseconds,
+    #                                        1.0 * unit.femtosecond)
+
+    integrator = openmm.RPMDIntegrator(2,
+                                       300 * unit.kelvin,
+                                       1.0 / unit.picoseconds,
+                                       1.0 * unit.femtosecond)
+
+    simulation = app.Simulation(modeller.topology, system, integrator)
+    simulation.context.setPositions(modeller.positions)
+    simulation.reporters.append(app.DCDReporter("output.dcd", 100))
+    simulation.reporters.append(
+        app.StateDataReporter(
+            stdout, 100, step=True, potentialEnergy=True, temperature=True, speed=True
+        )
+    )
+
+    # Minimize the energy
+    simulation.minimizeEnergy()
+
+    # Set the velocities to 300K and run 1000 steps
+    simulation.context.setVelocitiesToTemperature(300 * unit.kelvin)
+    simulation.step(1_000)
+
+
