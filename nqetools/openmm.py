029aa812b0d0f4bbaea3ea4e83dbf31e9714ad5f
Louie Slocombe <louies@hotmail.co.uk>
2025-11-12 21:37:03 -0700

    added MD workflow

--- nqetools/openmm.py
+++ nqetools/openmm.py
@@ -1,6 +1,12 @@
+from sys import stdout
+
+import matplotlib.pyplot as plt
 import numpy as np
 from openmm import openmm, app, unit
+from openmmtools.integrators import GeodesicBAOABIntegrator
 from pdbfixer import PDBFixer
+
+from .plotting import n_plot
 
 
 def fix_pdb(file_in, file_out, ph=7.0):
@@ -44,3 +50,123 @@ def init_beads(modeller, simulation, n_beads, perturb=0.002):
                     for p, (dx, dy, dz) in zip(pos0, jiggle)]
         simulation.integrator.setPositions(b, bead_pos * unit.nanometer)
         simulation.integrator.setVelocities(b, zero_velocities(n_atoms))
+
+
+def md_workflow(file_in,
+                ff='amber19-all.xml',  # charmm36_2024.xml amber19-all.xml
+                water_model='amber19/opc3.xml',  # charmm36_2024/tip5p.xml amber19/opc3.xml
+                padding=1.0,
+                temperature=300.0,
+                pressure=1.0,
+                friction_coeff=1.0,
+                time_step=0.004,
+                report_pdb=1_000,
+                report_std=1_000,
+                report_data=100,
+                file_out='output.pdb',
+                data_out='md_log.txt',
+                n_nvt=10_000,
+                n_npt=50_000,
+                box_shape='dodecahedron',
+                gbaoab=True,
+                platform='CPU',
+                ):
+    # Prepare system
+    pdb = app.PDBFile(file_in)
+    forcefield = app.ForceField(ff, water_model)
+    modeller = app.Modeller(pdb.topology, pdb.positions)
+    modeller.deleteWater()
+    modeller.addHydrogens(forcefield)
+
+    # Solvate
+    modeller.addSolvent(forcefield,
+                        padding=padding * unit.nanometer,
+                        boxShape=box_shape)
+
+    # Setup system and integrator
+    system = forcefield.createSystem(modeller.topology,
+                                     nonbondedMethod=app.PME,
+                                     nonbondedCutoff=1.0 * unit.nanometer,
+                                     constraints=app.HBonds)
+
+    if gbaoab:
+        integrator = GeodesicBAOABIntegrator(temperature * unit.kelvin,
+                                             friction_coeff / unit.picosecond,
+                                             time_step * unit.picoseconds)
+    else:
+        integrator = openmm.LangevinIntegrator(temperature * unit.kelvin,
+                                               friction_coeff / unit.picosecond,
+                                               time_step * unit.picoseconds)
+
+    platform = openmm.Platform.getPlatformByName(platform)
+    simulation = app.Simulation(modeller.topology, system, integrator, platform)
+    simulation.context.setPositions(modeller.positions)
+
+    # Local energy minimization
+    print("Minimizing energy", flush=True)
+    simulation.minimizeEnergy()
+
+    # Setup reporting
+    simulation.reporters.append(app.PDBReporter(file_out, report_pdb))
+
+    simulation.reporters.append(app.StateDataReporter(stdout,
+                                                      report_std,
+                                                      step=True,
+                                                      potentialEnergy=True,
+                                                      temperature=True))
+
+    simulation.reporters.append(app.StateDataReporter(data_out,
+                                                      report_data,
+                                                      step=True,
+                                                      time=True,
+                                                      potentialEnergy=True,
+                                                      kineticEnergy=True,
+                                                      totalEnergy=True,
+                                                      temperature=True,
+                                                      volume=True))
+
+    # NVT equilibration
+    print("Running NVT", flush=True)
+    simulation.step(n_nvt)
+
+    # NPT production MD
+    system.addForce(openmm.MonteCarloBarostat(pressure * unit.bar,
+                                              temperature * unit.kelvin))
+    simulation.context.reinitialize(preserveState=True)
+    print("Running NPT", flush=True)
+    simulation.step(n_npt)
+
+    return None
+
+
+def md_analysis(file_in='md_log.txt'):
+    # Analysis
+    data = np.loadtxt(file_in, delimiter=',')
+
+    step = data[:, 0]
+    time = data[:, 1]
+    potential_energy = data[:, 2]
+    kinetic_energy = data[:, 3]
+    total_energy = data[:, 4]
+    temperature = data[:, 5]
+    volume = data[:, 6]
+
+    plt.plot(time, potential_energy, lw=2)
+    n_plot('Time (ps)', 'Potential Energy (kJ/mol)')
+    plt.show()
+
+    plt.plot(time, kinetic_energy, lw=2)
+    n_plot('Time (ps)', 'Kinetic Energy (kJ/mol)')
+    plt.show()
+
+    plt.plot(time, total_energy, lw=2)
+    n_plot('Time (ps)', 'Total Energy (kJ/mol)')
+    plt.show()
+
+    plt.plot(time, temperature, lw=2)
+    n_plot('Time (ps)', 'Temperature (K)')
+    plt.show()
+
+    plt.plot(time, volume, lw=2)
+    n_plot('Time (ps)', 'Volume (nm^3)')
+    plt.show()
