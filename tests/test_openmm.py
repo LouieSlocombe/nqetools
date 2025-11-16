1c9aaacdb13d61f0a66a00975ef4cd7a9e59b06e
Louie Slocombe <louies@hotmail.co.uk>
2025-11-15 23:50:49 -0700

    WIP #255

--- tests/test_openmm.py
+++ tests/test_openmm.py
@@ -140,21 +140,21 @@ def test_openmm_ff_param():
     clean_pdb = "gt_wob_pol_clean.pdb"
 
     rm_ions = ['Na+', 'Cl-', 'NA']
-    residue_map = {'DGN': 'DG', 'DTN': 'DT'}
+    residue_map = {'DGN': 'DG', 'DTN': 'DT', 'GTP': 'LIG'}
 
     nqe.clean_ions_in_pdb(input_pdb, rm_ions, clean_pdb)
     nqe.relabel_residues_in_pdb(clean_pdb, residue_map, clean_pdb)
     nqe.remove_water_residues_in_pdb(clean_pdb, clean_pdb)
-    #nqe.fix_pdb(input_pdb, clean_pdb)
+    nqe.fix_pdb(input_pdb, clean_pdb)
 
-    # non_standard_mols = nqe.get_non_standard_residues(clean_pdb)
-    # n_ns = len(non_standard_mols)
-    # for mol in non_standard_mols:
-    #     Chem.SanitizeMol(mol)
-    #
-    # # write sdf files for each non-standard residue
-    # for i, mol in enumerate(non_standard_mols):
-    #     Chem.MolToMolFile(mol, f"non_standard_{i}.sdf")
+    non_standard_mols = nqe.get_non_standard_residues(clean_pdb)
+    n_ns = len(non_standard_mols)
+    for mol in non_standard_mols:
+        Chem.SanitizeMol(mol)
+
+    # write sdf files for each non-standard residue
+    for i, mol in enumerate(non_standard_mols):
+        Chem.MolToMolFile(mol, f"non_standard_{i}.sdf")
     #
     # # molecule = [Molecule.from_rdkit(mol, allow_undefined_stereo=True) for mol in non_standard_mols]
     # # molecule = Molecule.from_smiles(smi, allow_undefined_stereo=True)
@@ -163,7 +163,6 @@ def test_openmm_ff_param():
     # # Register the GAFF template generator
     # gaff = GAFFTemplateGenerator(molecules=molecule)
     # forcefield.registerTemplateGenerator(gaff.generator)
-
 
     pdbfile = app.PDBFile(clean_pdb)
     system = forcefield.createSystem(pdbfile.topology)
@@ -268,16 +267,223 @@ def test_openmm_gt_wobble():
 
 
 def test_openff():
-    from openff.toolkit import Molecule, Topology
+    import openmm.app as app
+    import openmm as mm
+    import openmm.unit as unit
+    from openff.toolkit.topology import Molecule
+    from openmmforcefields.generators import SystemGenerator
 
-    smis = [
-        '[H]-[O]-[C](-[H])(-[H])-[C@@]1(-[H])-[O]-[C@@](-[H])(-[n]2:[c](-[H]):[n]:[c]3:[c](=[O]):[n](-[H]):[c](-[N](-[H])-[H]):[n]:[c]:3:2)-[C](-[H])(-[H])-[C@]-1(-[H])-[O]-[H]',
-        '[H]-[O]-[C](-[H])(-[H])-[C@@]1(-[H])-[O]-[C@@](-[H])(-[n]2:[c](-[H]):[c](-[C](-[H])(-[H])-[H]):[c](=[O]):[n](-[H]):[c]:2=[O])-[C](-[H])(-[H])-[C@]-1(-[H])-[O]-[H]']
+    print("--- 1. Loading Input Files ---")
+    # Load the PDB file containing the entire complex (protein, DNA, ligand)
+    # This provides the topology and the initial positions.
 
-    mols = [Molecule.from_smiles(smi) for smi in smis]
-    complex = Topology.from_pdb("tests/data/pdb/gt_wob_solv.pdb", unique_molecules=mols)
+    pdb = app.PDBFile('gt_wob_pol_clean_fix.pdb')
 
-    complex.visualize()
+    # Load the ligand's SDF file.
+    # This provides the high-quality chemical definition (e.g., bond orders)
+    # for the OpenFF toolkit to parameterize.
+
+    ligand_molecule = Molecule.from_file('non_standard_0_fix.sdf', allow_undefined_stereo=True)
+
+    smi = '[H][O][C@@]1([H])[C]([H])([H])[C@]([H])([N]2[C]([H])([H])[N]([H])[C@]3([H])[C]2([H])[N]([H])[C@]([H])([N]([H])[H])[N]([H])[C@@]3([H])[O][H])[O][C@]1([H])[C]([H])([H])[O][P]([H])([O][H])([O][H])[O][P]([H])([O][H])([O][H])[O][P]([H])([O][H])([O][H])[O][H]'
+    smi = 'c1nc2c(n1[C@H]3[C@@H]([C@@H]([C@H](O3)CO[P@@](=O)(O)O[P@](=O)(O)OP(=O)(O)O)O)O)[nH]c(nc2=O)N'
+    ligand_molecule = Molecule.from_smiles(smi)
+
+    print(f"Loaded PDB with {pdb.topology.getNumAtoms()} atoms.")
+    print(f"Loaded ligand molecule: {ligand_molecule.to_smiles()}")
+
+    print("\n--- 2. Defining Force Fields ---")
+    # Define the standard force fields for protein and DNA.
+    # We use AMBER's ff14SB (protein), DNA.OL15 (DNA), and TIP3P (water).
+    standard_forcefields = [
+        'amber/ff14SB.xml',
+    ]
+
+    # Define the OpenFF force field for the small molecule.
+    # "openff-2.1.0.offxml" (Sage) is a robust, modern choice.
+    openff_forcefield = 'openff-2.1.0.offxml'
+
+    print("\n--- 3. Generating the Parameterized System ---")
+    # Initialize the SystemGenerator.
+    # This object will combine the force fields for us.
+    system_generator = SystemGenerator(
+        forcefields=standard_forcefields,
+        small_molecule_forcefield=openff_forcefield
+    )
+
+    # This is the key step. We provide the *topology* from the PDB
+    # and a list of *Molecule* objects we want to parameterize with OpenFF.
+    #
+    # The generator will:
+    # 1. Parameterize Protein/DNA/Water using the AMBER/TIP3P XML files.
+    # 2. Find the residue(s) in the PDB topology that are *not*
+    #    protein/DNA/water (i.e., our ligand).
+    # 3. Match these "unknown" residues against the `ligand_molecule`
+    #    based on chemical structure.
+    # 4. If a match is found, it applies the `openff_forcefield`
+    #    parameters to that residue.
+    system = system_generator.create_system(
+        pdb.topology,
+        molecules=[ligand_molecule]
+    )
+
+    print("Successfully created a combined OpenMM System.")
+    print(f"System has {system.getNumParticles()} particles.")
+    print(f"Using {system.getNumForces()} force groups:")
+    for i in range(system.getNumForces()):
+        print(f"  - Force {i}: {system.getForce(i).__class__.__name__}")
+
+    print("\n--- 4. Setting up the OpenMM Simulation ---")
+    # Set up a standard OpenMM simulation.
+    # We'll use a Langevin integrator for NVT dynamics.
+    integrator = mm.LangevinIntegrator(
+        300 * unit.kelvin,
+        1.0 / unit.picoseconds,
+        2.0 * unit.femtoseconds
+    )
+
+    # Create the Simulation object
+    simulation = app.Simulation(pdb.topology, system, integrator)
+
+    # Set the initial atomic positions from the PDB file
+    simulation.context.setPositions(pdb.positions)
+
+    print("Simulation object created successfully.")
+
+    print("\n--- 5. Running the Simulation ---")
+    # Add reporters to save data
+    # Save a DCD trajectory file every 1000 steps
+    simulation.reporters.append(
+        app.DCDReporter('trajectory.dcd', 1000)
+    )
+    # Print simulation state data (like energy and temp) to the console
+    simulation.reporters.append(
+        app.StateDataReporter(
+            'stdout',
+            1000,
+            step=True,
+            potentialEnergy=True,
+            temperature=True,
+            progress=True,
+            remainingTime=True,
+            speed=True,
+            totalSteps=5000
+        )
+    )
+
+    # First, minimize the energy to relax any clashes
+    print("Minimizing energy...")
+    simulation.minimizeEnergy()
+    print("Minimization complete.")
+
+    # Run 5,000 steps of NVT dynamics (10 ps)
+    print("Running NVT dynamics...")
+    simulation.step(5000)
+
+    print("\n--- Simulation Finished ---")
+    print("Check your directory for 'trajectory.dcd'.")
+
+
+def test_same():
+    from openff.toolkit.topology import Molecule, Topology
+    import openmm.app as app
+
+    # --- 1. Load your ligand from the SDF file ---
+    print("--- Loading from ligand.sdf ---")
+    # Use the same stereo flag from our previous fix
+    sdf_mol = Molecule.from_file('non_standard_0_fix.sdf', allow_undefined_stereo=True)
+    # Generate a canonical, isomeric SMILES string
+    sdf_smiles = sdf_mol.to_smiles(isomeric=True, explicit_hydrogens=True)
+
+    print(f"  SDF Molecule SMILES: {sdf_smiles}")
+    print(f"  SDF Molecule Atoms:  {sdf_mol.n_atoms}")
+
+    print("\n" + "-" * 30 + "\n")
+
+    # --- 2. Load your ligand from the PDB file ---
+    print("--- Loading from gt_wob_pol_clean_fix.pdb ---")
+
+    # Load the PDB
+    pdb = app.PDBFile('gt_wob_pol_clean_fix.pdb')
+
+    # Convert to an OpenFF Topology
+    # We pass the PDB's OpenMM topology to the OpenFF toolkit
+    off_topology = Topology.from_openmm(
+        pdb.topology,
+        unique_molecules=[]  # We don't provide any molecules here
+    )
+
+    # Find the GTP residue (Residue 344)
+    # We look for residue number 344 and name "GTP"
+    pdb_mol = None
+    for residue in off_topology.residues:
+        if residue.residue_number == 344 and residue.name == 'GTP':
+            # Found it! Get the corresponding OpenFF Molecule object
+            pdb_mol = off_topology.molecule_for_residue(residue)
+            break
+
+    if pdb_mol:
+        # Generate a canonical, isomeric SMILES string
+        pdb_smiles = pdb_mol.to_smiles(isomeric=True, explicit_hydrogens=True)
+
+        print(f"  PDB Molecule SMILES: {pdb_smiles}")
+        print(f"  PDB Molecule Atoms:  {pdb_mol.n_atoms}")
+    else:
+        print("  ERROR: Could not find residue 344 (GTP) in the PDB file.")
+
+
+def test_split():
+    from openff.toolkit.topology import Molecule
+    from openmm.app import ForceField
+    input_pdb = "tests/data/pdb/gt_wob_pol.pdb"
+    clean_pdb = "gt_wob_pol_clean.pdb"
+
+    rm_ions = ['Na+', 'Cl-', 'NA']
+    residue_map = {'DGN': 'DG', 'DTN': 'DT', 'GTP': 'LIG'}
+
+    smi = '[H]-[O]-[C@@]1(-[H])-[C](-[H])(-[H])-[C@](-[H])(-[N]2-[CH](-[H])-[NH]-[C@@H]3-[CH]-2-[NH]-[C@H](-[N](-[H])-[H])-[N](-[H])-[C@H]-3-[OH])-[O]-[C@]-1(-[H])-[C](-[H])(-[H])-[O]-[P-](-[O-])(-[O-])-[O]-[P-](-[O-])(-[O-])-[O]-[P-](-[O-])(-[O-])-[O-]'
+
+
+    nqe.clean_ions_in_pdb(input_pdb, rm_ions, clean_pdb)
+    nqe.relabel_residues_in_pdb(clean_pdb, residue_map, clean_pdb)
+    nqe.remove_water_residues_in_pdb(clean_pdb, clean_pdb)
+
+    non_standard_mols = nqe.get_non_standard_residues(clean_pdb)
+    # write sdf files for each non-standard residue
+    for i, mol in enumerate(non_standard_mols):
+        # print the number of atoms and bonds
+        #Chem.SanitizeMol(mol)
+        print(f"Non-standard residue {i}: {mol.GetNumAtoms()} atoms, {mol.GetNumBonds()} bonds")
+
+
+        print(Chem.MolToSmiles(mol, isomericSmiles=True, allBondsExplicit=True, allHsExplicit=True,canonical=True))
+        print(Chem.MolToInchi(mol))
+        Chem.MolToMolFile(mol, f"non_standard_{i}.sdf", kekulize=False)
+
+    # nqe.fix_pdb(clean_pdb, clean_pdb)
+    lig_mol = Molecule.from_file('non_standard_0.sdf', allow_undefined_stereo=True, file_format="SDF")
+    print(lig_mol.n_bonds, lig_mol.n_atoms, lig_mol.properties)
+
+    lig_mol = Molecule.from_rdkit(non_standard_mols[0], allow_undefined_stereo=True)
+    print(lig_mol.n_bonds, lig_mol.n_atoms, lig_mol.properties)
+
+
+    lig_mol = Molecule.from_smiles(smi, allow_undefined_stereo=True)
+    print(lig_mol.n_bonds, lig_mol.n_atoms, lig_mol.properties)
+
+    lig_mol = Molecule.from_inchi('InChI=1S/C10H30N5O13P3/c11-10-13-8-7(9(17)14-10)12-3-15(8)6-1-4(16)5(26-6)2-25-30(21,22)28-31(23,24)27-29(18,19)20/h4-10,12-14,16-24,29-31H,1-3,11H2/t4-,5+,6+,7+,8?,9?,10-/m0/s1', allow_undefined_stereo=True)
+    print(lig_mol.n_bonds, lig_mol.n_atoms, lig_mol.properties)
+
+
+    gaff = GAFFTemplateGenerator(molecules=lig_mol)
+    forcefield = ForceField(
+        'amber/ff14SB.xml',
+    )
+    forcefield.registerTemplateGenerator(gaff.generator)
+    from openmm.app import PDBFile
+
+    pdbfile = PDBFile("gt_wob_pol_clean.pdb")
+    system = forcefield.createSystem(pdbfile.topology)
 
 
 def test_openmm_constraints():
